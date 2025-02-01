bfcd4c617e3a21364124d754ea27fbf7806dacb0
Vivek Jaiswal <vj513714@gmail.com>
2025-02-01 23:34:13 +0000

    navbar update

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -109,8 +109,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head><link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com"/>
-<link href="https://fonts.googleapis.com/css2?family=Jersey+15&display=swap" rel="stylesheet"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" />
+<link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet"/>
 </head>
       <body className={`${inter.className} bg-gray-900 text-white`}>
         {/* <WalletProvider> */}
